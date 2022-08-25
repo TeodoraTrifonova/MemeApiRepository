2e62dcdddb6d8b2f868cc516582131ae50db55ea
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2022-08-25 15:30:54 +0300

    .

---
+++ MemeApiProject/src/main/java/tinqin/project/memeapiproject/MemeApiProjectApplication.java
@@ -0,0 +1,13 @@
+package tinqin.project.memeapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeApiProjectApplication.class, args);
+    }
+
+}
---
+++ MemeApiProject/src/test/java/tinqin/project/memeapiproject/MemeApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tinqin.project.memeapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
